5ffebabba0aadaa62410b4ad0d5aa1b052a27b20
Alexander <shymbabigmir@gmail.com>
2022-09-12 21:14:28 +0300

    project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Carousel
+        :navigation="true"
+        :pagination="true"
+        :startAutoPlay="true"
+        :timeout="5000"
+        class="carousel"
+        v-slot="{currentSlide}"
+    >
+      <Slide v-for="(slide, index) in carouselSlides" :key="index">
+        <div v-show="currentSlide === index + 1" class="slide-info">
+          <img :src="require(`../assets/${slide}.jpg`)">
+        </div>
+      </Slide>
+    </Carousel>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Carousel from "@/components/Carousel";
+import Slide from "@/components/Slide";
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    Carousel,
+    Slide
+  },
+   setup() {
+    const carouselSlides = ['img-1', 'img-2', 'img-3'];
+
+    return { carouselSlides };
+   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.carousel {
+  position: relative;
+  max-height: 100vh;
+  height: 100vh;
+
+  .slide-info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    max-height: 100%;
+    height: 100%;
+
+    img {
+      min-width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
